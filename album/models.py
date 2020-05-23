ab2e51bc85d4c3dc4ba9414526918e44436e7a51
ozanteoman <ozanteomandayanan@gmail.com>
2020-05-23 23:54:36 +0300

    added needs for tutorial

--- album/models.py
+++ album/models.py
@@ -11,6 +11,7 @@ class Artist(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=25)
+    is_visible = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
